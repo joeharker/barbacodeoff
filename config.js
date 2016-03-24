2e7261c488c57e60c3fd82fd023b571faf650968
joeharker <joeharker@gmail.com>
2016-03-24 11:42:21 -0600

    first check in

---
+++ config.js
@@ -0,0 +1,7 @@
+﻿var appConfig = (function () {
+    return {
+        googleAnalyticsKey: 'UA-67667605-5',
+        legacyOnlineOrderUrl: 'https://dev-order.chipotle.com/Feature',
+        apiBaseUrl: 'http://localhost:33877/api'
+    };
+})();
