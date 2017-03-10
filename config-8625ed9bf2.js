c82b946a1332161743354e84417269a19e07bbf3
joeharker <joeharker@gmail.com>
2017-03-10 12:52:20 -0700

    client side test one

---
+++ config-8625ed9bf2.js
@@ -0,0 +1,14 @@
+var appConfig = (function () {
+	return {
+		apiBaseUrl: 'api',
+		googleAnalyticsKey: 'UA-5654566-39',
+		legacyOnlineOrderUrl: 'https://order.chipotle.com',
+		environment: 'roo-phonegap',
+		payeezyApiKey: 'G9VX6CJrkE8kIk4WU75pnG9T4RAfC7zW',
+		payeezyJsSecurityKey: 'js-a7d5b6e6cbcef1b9b0f1cb5b572a3431a7d5b6e6cbcef1b9',
+		payeezyTaToken: 'LMUU',
+		payeezyJsHost: 'https://api.payeezy.com',
+		recaptchaKey: '6LfHjBgTAAAAACsApThgd6NG6085vWf-LFLE13a-',
+		urlWhiteList: 'chipotle.olo.com'
+	};
+})();
