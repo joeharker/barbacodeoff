b8461782a6e25322d03cb54cf6a021b4dc3842e8
joeharker <joeharker@gmail.com>
2017-03-10 13:34:52 -0700

    host test

---
+++ errorService.js
@@ -0,0 +1,92 @@
+'use strict';
+
+var errorService = (function () {
+	//pretty logger
+	function addCell(row, text, clas) {
+		var cel = row.insertCell();
+		var spn = document.createElement('span');
+		var txt = document.createTextNode(text);
+		if (clas !== undefined && clas !== null && clas !== '') {
+			spn.classList.add(clas);
+		}
+		spn.appendChild(txt);
+		cel.appendChild(spn);
+	}
+
+	function addRow(obj, key, table, maxRecursions) {
+		var value = obj[key];
+		var thatType = Object.prototype.toString.call(value); //more detail than typeof
+		var row = table.insertRow();
+
+		addCell(row, thatType, 'type');
+		addCell(row, key, 'name');
+
+		if (maxRecursions > 0 && typeof value === 'object' && value != null
+				&& key !== 'enabledPlugin' //this is a recursive link to the plug in that lists its self
+		) {
+			var cel = row.insertCell();
+			cel.innerHTML = objectToTable(value, --maxRecursions).outerHTML;
+		} else {
+			addCell(row, value, 'value');
+		}
+	}
+
+	function objectToTable(obj, maxRecursions) {
+		var table = document.createElement('table');
+		if (maxRecursions === undefined) {
+			maxRecursions = 20;
+		}
+
+		if (typeof obj === 'string') {
+			try {
+				addRow([JSON.parse(obj)], 0, table, maxRecursions);
+			} catch (e) {
+				addRow([obj], 0, table, maxRecursions);
+			}
+		} else {
+			Object.getOwnPropertyNames(obj).forEach(function (key) {
+				addRow(obj, key, table, maxRecursions);
+			});
+			for (var key in obj) {
+				if (!obj.hasOwnProperty(key)) {
+					addRow(obj, key, table, maxRecursions);
+				}
+			}
+		}
+
+		return table;
+	}
+
+	//public methods
+	function log(obj) {
+		console.log(obj);
+
+		//debug dump to screen for mobile testing
+		var debug = document.getElementById('debug');
+		if (debug) {
+			var table = document.createElement('table');
+			var thatType = Object.prototype.toString.call(obj); //more detail than typeof
+			var row = table.insertRow();
+			
+			addCell(row, thatType, 'type');
+			var cel = row.insertCell();
+			cel.innerHTML = objectToTable(obj).outerHTML;
+
+			debug.appendChild(table);
+			debug.style.display = 'block';
+		}
+	}
+
+	//event handler
+	window.onerror = function (msg, url, line, col, error) {
+		log(['Error event', error]);
+
+		//can add a service call to upload errors to a DB
+		//try catch the send so we dont end up in a reporting loop
+	};
+
+	return {
+		log: log
+	}
+
+})();
