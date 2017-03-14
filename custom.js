42ed3dc10291f4102abedcbc8c7c317d882a9272
joeharker <joeharker@gmail.com>
2017-03-14 15:20:09 -0600

    removed return call

--- custom.js
+++ custom.js
@@ -1,3 +1,1 @@
-﻿var img = document.querySelector('.header-icon');
-img.src = 'http://www.007dev.com/Images/icon.gif';
-return 'all images caching';
+﻿var img = document.querySelector('.header-icon'); img.src = 'http://www.007dev.com/Images/icon.gif';
