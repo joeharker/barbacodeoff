2908b9ba079871fca3e6f2e78294286be91b8abc
joeharker <joeharker@gmail.com>
2017-03-22 13:53:01 -0600

    cleanup

--- bootloader/bootloader.js
+++ bootloader/bootloader.js
@@ -16,7 +16,9 @@
 	}
 
 	function httpGet(url, callback) {
+		var async = true;
 		var xmlHttp = new XMLHttpRequest();
+
 		xmlHttp.onreadystatechange = function () {
 			if (xmlHttp.readyState === 4) {
 				if ([0, 200].indexOf(xmlHttp.status) !== -1) {
@@ -29,20 +31,21 @@
 		xmlHttp.onerror = function (e) {
 			log.write({ httpGetError: e });
 		};
-		xmlHttp.open('GET', url, true); // true for asynchronous 
+
+		xmlHttp.open('GET', url, async);
 		xmlHttp.send(null);
 	}
 
 	function runCustomScripts() {
-		//do not put a return in the custom javascript
-		//WARNING//inAppBrowserRef.executeScript({ file: getRoot() + 'custom.js' }, onScriptCallback);	//Docs say this works but it does not: 
+		//WARNING// do not put a return in the custom javascript
+		//WARNING// inAppBrowserRef.executeScript({ file: getRoot() + 'custom.js' }, onScriptCallback);	//Docs say this works but it does not: 
 		httpGet(getRoot() + 'custom.js', function (js) {
 			inAppBrowserRef.executeScript({ code: js });
 		});
 	}
 
-	function onInAppBrowserLoadError(params) {
-		log.write({ onInAppBrowserLoadError: params });
+	function onInAppBrowserLoadError(e) {
+		log.write({ onInAppBrowserLoadError: e });
 		onInAppBrowserExit();
 	}
 
@@ -52,22 +55,16 @@
 	}
 
 	function onInAppBrowserExit() {
-		//if they exit too far lets get them back into the InAppBrowser
-		if (inAppBrowserRef !== undefined) {
-			inAppBrowserRef = undefined;
-		}
 		if (!config.debug) {
 			navigator.app.exitApp();
 		}
 	}
 
 	function onDeviceReady() {
-		if (inAppBrowserRef === undefined) {
-			inAppBrowserRef = cordova.InAppBrowser.open(config.url + '?app=' + device.platform + config.version, '_blank', 'location=no,hidden=yes,clearcache=no,clearsessioncache=no,disallowoverscroll=yes,toolbar=' + config.iosBackButton);
-			inAppBrowserRef.addEventListener('loaderror', onInAppBrowserLoadError);
-			inAppBrowserRef.addEventListener('loadstop', onInAppBrowserLoadStop);
-			inAppBrowserRef.addEventListener('exit', onInAppBrowserExit);
-		}
+		inAppBrowserRef = cordova.InAppBrowser.open(config.url + '?app=' + device.platform + config.version, '_blank', 'location=no,hidden=yes,clearcache=no,clearsessioncache=no,disallowoverscroll=yes,toolbar=' + config.iosBackButton);
+		inAppBrowserRef.addEventListener('loaderror', onInAppBrowserLoadError);
+		inAppBrowserRef.addEventListener('loadstop', onInAppBrowserLoadStop);
+		inAppBrowserRef.addEventListener('exit', onInAppBrowserExit);
 	}
 
 	//when cordova is ready
--- custom.js
+++ custom.js
@@ -236,9 +236,6 @@ for (var i in images) {
 	img.src = 'https://order.chipotle.com/images/' + images[i];
 	img.width = 10;
 	img.height = 10;
-	img.style.position = 'absolute';
-	img.style.top = '10px';
-	img.style.left = '10px';
-	img.style.zIndex = '9999';
+
 	document.body.appendChild(img);
 }
