03600cf7a5edc1b57f365b91613b4e1c9bf68b48
joeharker <joeharker@gmail.com>
2017-03-22 13:34:48 -0600

    test

--- custom.js
+++ custom.js
@@ -236,10 +236,11 @@ var images = [
 for (var i in images) {
 	var img = document.createElement('img');
 	img.src = 'https://order.chipotle.com/images/' + images[i];
-	img.width = 1;
-	img.height = 1;
+	img.width = 10;
+	img.height = 10;
 	img.style.position = 'absolute';
-	img.style.bottom = '1px';
-	img.style.left = '1px';
+	img.style.bottom = '10px';
+	img.style.left = '10px';
+	img.style.zIndex = '999';
 	document.body.appendChild(img);
 }
